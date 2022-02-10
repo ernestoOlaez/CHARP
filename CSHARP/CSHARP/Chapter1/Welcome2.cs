46d44e02aa80a82ecc94736a061c1b84fe73978a
LAPTOP-4642O5JO\olaez <olaez_ernesto@hotmail.com>
2022-02-09 22:09:31 -0600

    Add code

---
+++ CSHARP/CSHARP/Chapter1/Welcome2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP.Chapter1
+{
+    internal class Welcome2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to ");
+            Console.WriteLine("C# Programming!");
+        }
+    }
+}
---
+++ CSHARP/CSHARP/Chapter1/Welcome3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP.Chapter1
+{
+    internal class Welcome3
+    {
+        //svm
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Giselle \nis \nlearning \nenglish!..");
+        }
+       
+    }
+}
